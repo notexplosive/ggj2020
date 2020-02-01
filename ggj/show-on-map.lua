c2667e2860d49ab59c9c5307807607d5cc56092d
Wyatt Chapman <thenotexplosive@gmail.com>
2020-02-01 11:25:18 -0800

    Map improvements

--- ggj/show-on-map.lua
+++ ggj/show-on-map.lua
@@ -10,8 +10,13 @@ end
 function ShowOnMap:drawMapIcon(x, y, scale)
     love.graphics.setColor(self.color or {1, 1, 1, 1})
     local radius = 5
+
     if self.radius then
         radius = self.radius * scale
+    else
+        if self.actor.CollideRadius then
+            radius = self.actor.CollideRadius:get() * scale
+        end
     end
     love.graphics.circle("fill", x, y, radius)
 end
