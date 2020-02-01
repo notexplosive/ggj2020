80a573b3b4b79c5a93da74633a6640d5615584da
Wyatt Chapman <thenotexplosive@gmail.com>
2020-01-31 17:54:52 -0800

    Initial commit

---
+++ ggj/player.lua
@@ -0,0 +1,17 @@
+local Player = {}
+
+registerComponent(Player,'Player')
+
+function Player:setup()
+    
+end
+
+function Player:draw(x,y)
+    
+end
+
+function Player:update(dt)
+    
+end
+
+return Player
