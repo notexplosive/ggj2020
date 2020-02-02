4b54c6290b8bf5614fb69f5a9b6e2cf96d218723
Wyatt Chapman <thenotexplosive@gmail.com>
2020-02-01 16:49:57 -0800

    SpriteRenderer API change

--- nx/components/core/sprite-renderer.lua
+++ nx/components/core/sprite-renderer.lua
@@ -111,6 +111,10 @@ function SpriteRenderer:setLoop(b)
     self.actor.PlayHead:setLoop(b)
 end
 
+function SpriteRenderer:setFrame(frame)
+    self.actor.AnimationFrameTracker:set(frame)
+end
+
 function SpriteRenderer:setFrame(index)
     self.actor.AnimationFrameTracker:set(index)
 end
