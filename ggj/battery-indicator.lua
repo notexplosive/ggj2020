5250f4b210b6e413e95d3cf50e5064a69b64a3e8
Andy Brown <andybrown1987@hotmail.com>
2020-02-01 17:08:23 -0800

    Battery status

--- ggj/battery-indicator.lua
+++ ggj/battery-indicator.lua
@@ -1,6 +1,6 @@
 local BatteryIndicator = {}
 
-registerComponent(BatteryIndicator, "BatteryIndicator", {"TextRenderer", "PlayerRef"})
+registerComponent(BatteryIndicator, "BatteryIndicator", {"PlayerRef"})
 
 function BatteryIndicator:setup()
 end
@@ -8,19 +8,44 @@ end
 function BatteryIndicator:awake()
     local sp = self.actor:addComponent(Components.SpriteRenderer, "battery", "all", 2)
     --sp:setLoop(false)
+    local actor = self.actor:scene():addActor()
+    actor:setPos(160, 48)
+    actor:addComponent(Components.SpriteRenderer, "charge-status", "all", 2)
+    self.chargeActor = actor
 end
 
 function BatteryIndicator:draw(x, y)
-    love.graphics.circle("fill", x, y, 5)
 end
 
 function BatteryIndicator:update(dt)
     local player = self.actor.PlayerRef:get()
     if player then
-        local status = math.floor(player.Battery:getPercent() * 100) .. ", " .. player.Battery:calculateUsage()
-        self.actor.TextRenderer.text = status
+        --local status = math.floor(player.Battery:getPercent() * 100) .. ", " .. player.Battery:calculateUsage()
+        --self.actor.TextRenderer.text = status
+        local batteryTarget = self:calculateBatteryFrame(player.Battery:calculateUsage())
         self.actor.PlayHead:set((self.actor.PlayHead.maxTime-0.1)*player.Battery:getPercent())
+        self.chargeActor.SpriteRenderer:setFrame(batteryTarget)
     end
+end
+
+function BatteryIndicator:calculateBatteryFrame(batteryUsage)
+    local chargeDraw = batteryUsage * -1
+    local frame = 0
+    if chargeDraw > 4 then
+        frame = 6
+    elseif chargeDraw > 2 then
+        frame = 5
+    elseif chargeDraw > 0.25 then
+        frame = 4
+    elseif chargeDraw > -0.25 then
+        frame = 3
+    elseif chargeDraw > -2 then
+        frame = 2
+    elseif chargeDraw > -4 then
+        frame = 1
+    end
+    debugLog(frame)
+    return frame
 end
 
 return BatteryIndicator
