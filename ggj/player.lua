0be023143a8306062243e40e0c27b953101d5a19
Wyatt Chapman <thenotexplosive@gmail.com>
2020-02-01 17:26:28 -0800

    Take damage

--- ggj/player.lua
+++ ggj/player.lua
@@ -4,6 +4,41 @@ registerComponent(Player, "Player")
 
 function Player:onCollide(other)
     statusLog("Hull damage!")
+
+    local allWidgets = uiScene:getAllActorsWith(Components.Widget)
+
+    local enabledWidgets = {}
+    for i, widget in ipairs(allWidgets) do
+        if not widget.Widget.isDisabled then
+            append(enabledWidgets, widget)
+        end
+    end
+
+    table.sort(
+        enabledWidgets,
+        function(act1, act2)
+            return act1.Widget.priority < act2.Widget.priority
+        end
+    )
+
+    if #enabledWidgets == 0 then
+        debugLog("GAME OVER")
+        return
+    end
+
+    local lowestPriority = enabledWidgets[1].Widget.priority
+
+    local widgetsToDamage = {}
+    for i, widget in ipairs(enabledWidgets) do
+        if widget.Widget.priority == lowestPriority then
+            append(widgetsToDamage, widget)
+        end
+    end
+
+    if #widgetsToDamage > 0 then
+        local actor = getRandom(widgetsToDamage)
+        actor.Widget:disable()
+    end
 end
 
 return Player
