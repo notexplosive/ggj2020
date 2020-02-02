2cf92cf445d71dcb2f4326773ee89a9574a739a2
Wyatt Chapman <thenotexplosive@gmail.com>
2020-02-01 20:12:19 -0800

    Embiggened the map

--- ggj/black-screen.lua
+++ ggj/black-screen.lua
@@ -3,7 +3,7 @@ local BlackScreen = {}
 registerComponent(BlackScreen, "BlackScreen")
 
 function BlackScreen:draw(x, y)
-    love.graphics.setColor(0, 0, 0, 0.5)
+    love.graphics.setColor(0, 0, 0, 1)
     love.graphics.rectangle("fill", 0, 0, love.graphics.getDimensions())
 end
 
