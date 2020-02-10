1927bb2c14104abcac65ba142a5d55f421ddd807
Andy Brown <andybrown1987@hotmail.com>
2020-02-09 23:50:33 -0800

    Minor star tweak

--- ggj/spawn-stars.lua
+++ ggj/spawn-stars.lua
@@ -10,9 +10,13 @@ function SpawnStars:setup(layer)
         local y = love.math.random(-height / 2, height / 2)
         actor:setPos(x, y)
 
-        
+        local minTime = 2
+        local maxTime = 10
+
         if (love.math.random() > 0.95) then
             local sp = actor:addComponent(Components.SpriteRenderer, "star4", "all")
+            minTime = 10
+            maxTime = 20
         elseif (love.math.random() > 0.85) then
             local sp = actor:addComponent(Components.SpriteRenderer, "star3", "all")
         elseif (love.math.random() > 0.70) then
@@ -22,7 +26,7 @@ function SpawnStars:setup(layer)
         end
 
         actor.PlayHead:pause()
-        actor:addComponent(Components.RandomLoopTime, 1, 10)
+        actor:addComponent(Components.RandomLoopTime, minTime, maxTime)
         --actor.PlayHead.time = love.math.random(0, actor.PlayHead.maxTime * 10) / 10
         actor:addComponent(Components.Parent, self.actor)
         actor.SpriteRenderer.color[4] = 1 / layer
